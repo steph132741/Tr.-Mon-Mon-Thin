915a34c81644ab79214aec3f268acc7259b634d6
steph132741 <yuwady.yas@gmail.com>
2025-10-29 14:27:07 +0630

    Add Bubble Sort implementation

---
+++ Bubble.py
@@ -0,0 +1,16 @@
+class BubbleSort:
+    @staticmethod
+    def sort(arr):
+        n = len(arr)
+        for i in range(n - 1):
+            for j in range(n - 1 - i):
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+print("Unsorted array:", arr)
+
+BubbleSort.sort(arr)
+
+print("Sorted array:", arr)
